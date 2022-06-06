5b7a59c9285cf1a7d06243d135cf1c083fb17901
MARTIKRISO <mshkodrov@gmail.com>
2022-06-06 19:42:45 +0300

    Added DB Connection Info Added DB Connection in DataLayer

---
+++ DataLayer/MVRSystemDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class OnlineShopDBContext : DbContext
+    {
+        public OnlineShopDBContext() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"id=MVRSystemDB.mssql.somee.com;packet size=4096;user id=mshkodrov_SQLLogin_1;pwd=qrktrucpmy;data source=MVRSystemDB.mssql.somee.com;persist security info=False;initial catalog=MVRSystemDB");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        //public DbSet<Customer> Customers { get; set; }
+
+        //public DbSet<Product> Products { get; set; }
+
+        //public DbSet<Order> Orders { get; set; }
+    }
+}
