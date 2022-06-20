d289e951f422b4c7f585a503edc52fa8f9200099
MARTIKRISO <mshkodrov@gmail.com>
2022-06-20 10:03:13 +0300

    Testing P1

--- TestingLayer/DrivingLicenseTesting.cs
+++ TestingLayer/DrivingLicenseTesting.cs
@@ -9,7 +9,8 @@
         [TestInitialize]
         public void Init()
         {
-            Card CARD = new Card("custom3", "customegn", "none", "customfname", "custommname", "customlname", "m", "01.01.2001", "31.12.2050", "Plovdiv", "Plovdiv", "Plovdiv", "Plovdiv", "ul. Spas Tzilkov 5", 203, "black", "MVR Sofiq", "02.01.2021", "213213", "3123124", "3123123");
+
+            Card CARD = new Card("custom2", "customegn", "none", "customfname", "custommname", "customlname", "m", "01.01.2001", "31.12.2050", "Plovdiv", "Plovdiv", "Plovdiv", "Plovdiv", "ul. Spas Tzilkov 5", 203, "black", "MVR Sofiq", "02.01.2021", "213213", "3123124", "3123123");
             DrivingLicense LICENSE = new DrivingLicense("customid", "customegn", CARD, 69, "B");
             this.LICENSE = LICENSE;
             this.CARD = CARD;
@@ -58,7 +59,7 @@
         [TestMethod]
         public void UpdateTest()
         {
-            Card card = new Card("custom2", "custom2gn", "none", "customfname", "custommname", "customlname", "m", "01.01.2001", "31.12.2050", "Plovdiv", "Plovdiv", "Plovdiv", "Plovdiv", "ul. Spas Tzilkov 5", 203, "black", "MVR Sofiq", "02.01.2021", "213213", "3123124", "3123123");
+            Card card = new Card("custom3", "custom2gn", "none", "customfname", "custommname", "customlname", "m", "01.01.2001", "31.12.2050", "Plovdiv", "Plovdiv", "Plovdiv", "Plovdiv", "ul. Spas Tzilkov 5", 203, "black", "MVR Sofiq", "02.01.2021", "213213", "3123124", "3123123");
             DrivingLicense newlic = new DrivingLicense("customid", "custom2gn", card, 69, "B");
             ctx.Update(newlic);
             Assert.AreEqual("customegn2", ctx.Read("customid").EGN);
